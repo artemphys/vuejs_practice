e407315a14ddad8e0fbe038f5a11c4b5f5bcf0cf
artemphys <artemphys@gmail.com>
2024-04-28 18:12:34 +0200

    Added ModalComponent

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import ModalComponent from '@/components/ModalComponent.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <ModalComponent />
   </main>
 </template>
